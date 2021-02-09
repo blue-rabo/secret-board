616d560eb8e1cf4fb3d09a4628bcbc201f855026
aozametatuki <akomatsu1224@gmail.com>
2021-02-09 23:06:57 +0900

    認証機能を実装

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 'use strict';
-const http = require('http');
-const router = require('./lib/router');
 
-const server = http.createServer((req, res) => {
-  router.route(req, res);
-}).on('error', (e) => {
-  console.error('Server Error', e);
-}).on('clientError', (e) => {
-  console.error('Client Error', e);
+// httpのモジュールを読み込む
+const http = require('http');
+// http-authのモジュールを読み込む
+const auth = require('http-auth');
+// router.jsを読み込む
+const router = require('./lib/router');
+// ファイルの情報を利用したBasic認証
+const basic = auth.basic({
+    realm: 'Enter username and password.',
+    file: './users.htpasswd'
 });
 
-const port = 8000;
+// サーバーを作成
+const server = http.createServer(basic, (req, res) => {
+    // 処理を振り分ける
+    router.route(req, res);
+// サーバエラーのエラーログを出力
+}).on('error', (e) => {
+    console.error(`Server Error`, e);
+// クライアントエラーのエラーログを出力
+}).on('clientError', (e) => {
+    console.error(`Client Error`, e);
+});
+
+// httpが起動するポートを設定
+const port = process.env.PORT || 8000;
+// サーバーを起動
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+	  // ログを出力
+    console.info(`Listening on ${port}`);
 });
