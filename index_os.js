1659870b6a1f026f25c94d22afda37bcf15c83c7
aozametatuki <akomatsu1224@gmail.com>
2021-02-14 21:01:12 +0900

    OSコマンドを実行しないように修正

--- index_os.js
+++ index_os.js
@@ -2,8 +2,8 @@
 
 // httpのモジュールを読み込む
 const http = require('http');
-// child_processのモジュールを読み込む
-const cp = require('child_process');
+// // child_processのモジュールを読み込む
+// const cp = require('child_process');
 
 // httpサーバを作成
 const server = http.createServer((req, res) => {
@@ -14,7 +14,9 @@ const server = http.createServer((req, res) => {
       'Content-Type': 'text/plain; charset=utf-8'
     });
     // OSのechoコマンドの実行結果をレスポンスとして返す
-    res.end(cp.execSync(`echo ${path}`));
+    // res.end(cp.execSync(`echo ${path}`));
+    // OSコマンドを実行しないように修正
+    res.end(path);
 });
 
 // httpが起動するポートを設定
